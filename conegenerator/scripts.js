c66023a27ca5668016e5ff57db685ea177f3b258
Tyler Garner <tylergarner@gmail.com>
2017-06-19 01:49:25 -0600

    its so laaaaate

--- conegenerator/scripts.js
+++ conegenerator/scripts.js
@@ -1,5 +1,5 @@
 var coneDisplay = $('.flavorbox')
-var flavorDisplay = $('.flavorchecklist')
+var flavorDisplay = $('input:checked')
 var cardDiv =   $('.card-box')
 
 
@@ -9,19 +9,28 @@ function Cone(name, flavor) {
 }
 
 function makeCone() {
-  var cone = new Cone(coneDisplay.val(), flavorDisplay);
+  var cone = new Cone(coneDisplay.val(), $('input:checked'));
   cardDiv.append(coneOutput(cone))
 }
 
 function coneOutput(newCone) {
-  $('.card-box').append(`<div class="cone-card">
-          <h3 class="conedisplay">${newCone.name}</h3>
-        <p class="flavordisplay">${newCone.flavor}</p>
-      </div>`)
+  $('.card-box').append(` <div class="cone-card">
+      <div class="card-top"><h3 class="conedisplay">${newCone.name}</h3><p>Flavors</p></div>
+    <div class="card-bottom"></div>
+</div>`)
 }
+
+// function getChecked(check) {
+//   var $('.flavorchecklist')
+
+// function checkChecks() {
+// var checkList = $('.checkbox')
+// checkList.forEach(function object, index) {
+//   console.log(checklist)
+//   }
+// }
 
 
 $('#submit').on('click', function() {
   makeCone();
-  console.log($('.flavorchecklist:checked'))
 });
